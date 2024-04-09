2d943b225596918bce22880bc901470ee7bd349c
Douglas Ribeiro da Rosa <douglasribeirodarosa@gmail.com>
2024-04-08 21:37:37 -0300

    recipes

--- recipes/views.py
+++ recipes/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context={'name': 'Douglas Ribeiro', })
+    return render(request, 'recipes/pages/home.html', context={'name': 'Douglas Ribeiro', })
